a1a961298faea4eb67184aa44b8cd738f0517c71
Andrew-liu <1095511864@qq.com>
2015-09-09 20:43:18 +0800

    invert binary tree

---
+++ InvertBinaryTree.cpp
@@ -0,0 +1,26 @@
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root == NULL) {
+            return NULL;
+        }
+        // 交换当前节点的左右指针
+        TreeNode *temp = root->right;
+        root->right = root->left;
+        root->left = temp;
+        // 递归调用
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
+};
