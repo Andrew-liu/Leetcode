3d94300d178a74c70b7334c444b132700472562f
Andrew-liu <1095511864@qq.com>
2015-09-11 14:56:58 +0800

    reverse linked list

---
+++ ReverseLinkedList.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL) {
+            return NULL;
+        }
+        //只有一个节点
+        if (head->next == NULL) {
+            return head;
+        }
+        ListNode* pre = NULL;
+        ListNode* cur = head;
+        ListNode* end = head->next;
+        while(cur->next != NULL) {
+            cur->next = pre;
+            pre = cur;
+            cur = end; 
+            end = end->next;
+        }
+        // 最后一次循环时 cur并没有被翻转, 所以需要最后处理一下cur所指向的节点
+        // cur是最后一个节点, 翻转后变成了头结点
+        cur->next = pre;
+        head = cur;
+        return head;
+    }
+};
