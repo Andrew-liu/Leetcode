cbdfacd613aeb4c8fb1f84f77f4e9c0892f38868
Andrew-liu <1095511864@qq.com>
2015-09-19 19:45:27 +0800

    Remove Nth Node From End of List

---
+++ RemoveNthNodeFromEndOfList.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ * 
+ * 思路: 首先遍历链表获取长度, 然后找到正向的位置, 再遍历找到前驱
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if(head == NULL) {
+            return head;
+        }
+        int len = 0;
+        ListNode* cur = head;
+        while(cur != NULL) {
+            cur = cur->next; 
+            len++;
+        }
+        int pre_loc = len - n; //获取要删除节点前驱的位置
+        cur = head;
+        if (pre_loc == 0) {  //要删除的为第一个节点
+            head = head->next;
+            return head;
+        }
+        pre_loc--; //减一才能够找到前驱节点的位置, 不建议找到的是删除节点的位置
+        while(pre_loc != 0) {
+            pre_loc--;
+            cur = cur->next;
+        } //跳出循环后, cur指向删除节点的前驱
+        cur->next = cur->next->next;
+        return head;
+    }
+};
