60a2958a00509e24af7cd325317e64384c299f05
Andrew-liu <1095511864@qq.com>
2015-09-21 22:07:33 +0800

    BinaryTreeInorderTraversal

---
+++ BinaryTreeInorderTraversal.cpp
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ * 
+ * 中序遍历 (左->中->右)
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if (root == NULL) {
+            return res;
+        }
+        inorder(root, res);
+        return res;
+    }
+    void inorder(TreeNode* root, vector<int>& res) {
+        if(root == NULL) {
+            return;
+        }
+        inorder(root->left, res);
+        res.push_back(root->val);
+        inorder(root->right, res);
+    }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ * 
+ * 非递归的实现非常类似先序遍历, 不断将左节点入栈,  然后当前栈顶节点top并pop, 然后进入vector. 并循环判断当前节点的右子树
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if (root == NULL) {
+            return res;
+        }
+        stack<TreeNode*> temp; 
+        TreeNode* cur = root;
+        while(cur != NULL || !temp.empty()) {
+            while(cur != NULL) {
+                temp.push(cur);
+                cur = cur->left; //所有左侧的节点入栈
+            }
+            cur = temp.top();
+            temp.pop();
+            res.push_back(cur->val);
+            cur = cur->right;
+        }
+        return res;
+    }
+};
