f7a655664196977dafa931d712d7379068058ef1
Andrew-liu <1095511864@qq.com>
2015-09-25 21:46:20 +0800

    FindMinimuminRotatedSortedArray

--- FindMinimuminRotatedSortedArray.cpp
+++ FindMinimuminRotatedSortedArray.cpp
@@ -10,3 +10,21 @@ public:
         return nums[0];
     }
 };
+
+// 二分查找
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int temp = nums[0], left = 0, right = nums.size() - 1;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(nums[mid] >= nums[right]) { //说明后半段比较小, 则较小的数字在后半段
+                left = mid + 1;
+            } else {  //说明比较小的在前半段, 
+                right = mid - 1;
+            }
+            temp = min(temp, nums[mid]);
+        }
+        return temp;
+    }
+};
