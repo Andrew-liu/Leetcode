9305b116d71d790f7183b58dcf7435de7172c34e
Andrew-liu <1095511864@qq.com>
2015-10-24 22:42:49 +0800

    NumberOfIslands

---
+++ NumberOfIslands.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        if(grid.empty() || grid[0].empty()) {
+            return 0;
+        }
+        int m = grid.size(), n = grid[0].size(), res = 0;
+        vector<vector<bool> > visited(m, vector<bool>(n, false));  // 用于判断当前的位置是否已经访问过
+        for(int i = 0; i < m; ++i) {  //遍历所有的节点
+            for(int j = 0; j < n; ++j) {
+                if(grid[i][j] == '1' && visited[i][j] == false) {
+                    DFS(grid, visited, i, j);
+                    ++res;
+                }
+            }
+        }
+        return res;
+    }
+    void DFS(vector<vector<char> >& grid, vector<vector<bool> >& visited, int x, int y) {
+        if(x < 0 || x >= grid.size() || y < 0 || y >= grid[0].size()) //四种边界情况
+            return;
+        if(grid[x][y] != '1' || visited[x][y]) 
+            return;
+        visited[x][y] = true;
+        DFS(grid, visited, x - 1, y);
+        DFS(grid, visited, x + 1, y);
+        DFS(grid, visited, x, y - 1);
+        DFS(grid, visited, x, y + 1);
+    }
+};
