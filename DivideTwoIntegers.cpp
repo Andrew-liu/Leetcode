5aaa23bfdb1a5db058dac1bbdef6bdd7b9bd83ee
Andrew-liu <1095511864@qq.com>
2015-11-15 15:23:54 +0800

    DivideTwoIntegers

--- DivideTwoIntegers.cpp
+++ DivideTwoIntegers.cpp
@@ -6,7 +6,7 @@ public:
         long m = labs(dividend), n = labs(divisor), res = 0;
         long sign = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
         if (n == 1) return m * sign;
-        while (m >= n) {
+        while (m >= n) {  //被除数大于除数则进入循环
             long k = n;
             int i = 0;
             while (k <= m) {
