c0028aae8c8359339bac62b2b68320e9b85fe8a1
Andrew-liu <1095511864@qq.com>
2016-04-09 21:49:47 +0800

    回文链表新解法

--- PalindromeLinkedList.cpp
+++ PalindromeLinkedList.cpp
@@ -42,6 +42,47 @@ public:
     }
 };
 
+ /*
+ 1. 可是申请额外空间, 则前半段入栈, 和后半段出栈比较
+ 2. 不可申请, 前半段reverse, 然后分别从两段的头部比较
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if(head == NULL) {
+            return true;
+        }
+        ListNode* cur = head;
+        int len = 0;
+        while(cur != NULL) {
+            len++;
+            cur = cur->next;
+        }
+        int half = len / 2;
+        int temp = 0;
+        ListNode* pre = NULL;
+        ListNode* tmp = NULL;
+        cur = head;
+        while(temp++ < half) {  //翻转前半段
+            tmp = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        if(len % 2 != 0) { //当为奇数时, 指向中间值
+            cur = cur->next;
+        }
+        while(pre && cur) {
+            if(pre->val != cur->val) {
+                return false;
+            }
+            pre = pre->next;
+            cur = cur->next;
+        }
+        return true;
+    }
+};
+
 int main(int argc, char** argv) {
     ListNode root(1);
     ListNode foo1(2);
