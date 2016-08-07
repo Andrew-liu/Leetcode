758b53d99156e8ad84545b6ba309f0f6725621aa
Andrew-liu <1095511864@qq.com>
2016-08-07 22:33:13 +0800

    100. Same Tree add comment

--- SameTree.cpp
+++ SameTree.cpp
@@ -10,39 +10,13 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (p == NULL && q == NULL) {  //p和q都为空时
-            return true;
-        } else if (p && q) { // p和q都不为空时
-            if (!isEqual(p->val, q->val)) {
-                return false;
-            } 
-            if (isSameTree(p->left, q->left) == false)
-                return false;
-            if (isSameTree(p->right, q->right) == false)
-                return false;
-            return true;
-        } else {  // p和q一个为空, 一个不为空
-            return false;
-        }
-    }
-    bool isEqual(int val1, int val2) {
-        return val1 == val2 ? true : false;
-    }
-};
-
-class Solution {
-public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (!p && !q) {  //两个极点都为空
+        if (!p && !q) {  //两个节点都为空, 表示已经都了叶子节点的后继(并且考虑到1, 0这种特殊case)
             return true;
         }
         if (!p || !q) {  //一个节点为空, 一个节点不为空
             return false;
         }
-        // 两个节点都不为空, 则考虑他的左右子节点
+        // 两个节点都不为空, 则判断节点的值是否相同, 并且递归的判断左子树和右子树
         return (p->val == q->val) && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
-    }
-    bool isEqual(int val1, int val2) {
-        return val1 == val2 ? true : false;
     }
 };
