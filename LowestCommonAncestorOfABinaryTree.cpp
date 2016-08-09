75829909e6aa52942ea24e7c67dd6a1ead3f94ca
Andrew-liu <1095511864@qq.com>
2016-08-10 00:00:22 +0800

    235. Lowest Common Ancestor of a Binary Search Tree add comment

--- LowestCommonAncestorOfABinaryTree.cpp
+++ LowestCommonAncestorOfABinaryTree.cpp
@@ -7,17 +7,24 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+ /*
+  * 题目大意:给的是二叉搜索树，寻找其中两个给定节点的最近公共祖先（LCA）。
+  * 根据维基百科对LCA的定义：
+  * 节点v与w的最近公共祖先是树T上同时拥有v与w作为后继的最低节点（注意这句话: 允许将一个节点当做其本身的后继）
+  */
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (!root || p == root || q == root) {
+        if (root == NULL) {
+            return NULL;
+        }
+        if (p->val < root->val && q->val < root->val) {
+            return lowestCommonAncestor(root->left, p, q);
+        } else if (p->val > root->val && q->val > root->val) {
+            return lowestCommonAncestor(root->right, p, q);
+        } else {
+            // 一个大于一个小于, 则公共祖先就是当前这个子树的根节点
             return root;
         }
-        TreeNode* left = lowestCommonAncestor(root->left, p, q);
-        TreeNode* right = lowestCommonAncestor(root->right, p, q);
-        if(left && right) {
-            return root;
-        }
-        return left ? left : right;
     }
 };
