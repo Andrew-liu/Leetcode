26b0ca5cf5841e485bd3cbb4e68dbc1a40d0aac9
Andrew-liu <1095511864@qq.com>
2016-08-10 22:55:57 +0800

    257. Binary Tree Paths add new method

--- BinaryTreePaths.cpp
+++ BinaryTreePaths.cpp
@@ -47,16 +47,50 @@ public:
     }
 };
 
+ /*
+  * 思路: 深度优先遍历的变种题, 应该使用一个变量还缓存已经走过的路径
+  * 分三种情况, 1. 左右子树均为空, 表示已经到叶节点, 此条路径结束, 
+  * 2. 左子树为空, 则递归深度遍历右子树, 3. 右子树为空, 则递归的遍历左子树
+  */
+class Solution1 {
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> paths;
+        vector<int> nodes;
+        helper(root, nodes, paths);
+        return paths;
+    }
+    void helper(TreeNode* node, vector<int>& nodes, vector<string>& paths) {
+        if (node == NULL) { // 节点为空时
+            return;
+        }
+        if (node->left == NULL && node->right == NULL) {
+            stringstream ss;
+            for (int i = 0; i < nodes.size(); ++i) {
+                ss << nodes[i] << "->";
+            }
+            ss << node->val; // 放入最后一个节点
+            paths.push_back(ss.str());
+            return;
+        }
+        nodes.push_back(node->val);
+        helper(node->left, nodes, paths);
+        helper(node->right, nodes, paths);
+        nodes.pop_back();  // 回退
+    }
+};
+
 int main(int argc, char** argv) {
     TreeNode root(1);
     TreeNode left(2);
     TreeNode right(3);
     root.left = &left;
     root.right = &right;
-    Solution s;
+    Solution1 s;
     vector<string> temp = s.binaryTreePaths(&root);
     for(std::vector<std::string>::size_type ix = 0;
         ix != temp.size(); ++ix) {
-        cout << temp[ix] << endl;
+        cout << "index: " << ix << " value: " << temp[ix] << endl;
     }
+    std::cout << "size: " << temp.size() << std::endl;
 }
