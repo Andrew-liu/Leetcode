466cf8e7aa63bc0a09255f10bc12af51efd3e710
Andrew-liu <1095511864@qq.com>
2016-09-22 22:10:04 +0800

    8. String to Integer (atoi) update

--- StringToInteger.cpp
+++ StringToInteger.cpp
@@ -26,6 +26,38 @@ public:
         }
     }
 };
+
+// 注意空格, 正负号和溢出
+class Solution {
+public:
+    int myAtoi(string str) {
+        long res = 0;
+        int size = str.size();
+        int symbol = 1;
+        
+        for(int i = 0; i < size;) {
+            while(str[i] == ' ') {
+                i++;
+            }
+            if(str[i] == '-' || str[i] == '+') {
+                symbol = str[i] =='-' ? -1 : 1;
+                i++;
+            }
+            while(i < size && isdigit(str[i])) {
+                res = res * 10 + (str[i++] - '0');
+                if(res * symbol >= INT_MAX) {
+                    return INT_MAX;
+                }
+                if(res * symbol <= INT_MIN) {
+                    return INT_MIN;
+                }
+            }
+            return (int)symbol * res;
+        }
+        return (int)symbol * res;
+    }
+};
+
 int main(int argc, char **argv) {
     Solution s;
     cout << s.myAtoi("2147483648") << endl;
